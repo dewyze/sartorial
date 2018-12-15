2fbf39cdff5ff589d9484f37a29646a51f2e535f
John DeWyze <john.dewyze@gmail.com>
2018-12-15 09:58:53 -0600

    Create table for structured html with lists

---
+++ _plugins/structured_html_tag.rb
@@ -0,0 +1,32 @@
+module Jekyll
+  class StructuredHtmlTag < Liquid::Tag
+    def initialize(tag_name, source, tokens)
+      super
+      @source = source
+    end
+
+    def render(context)
+      @data = context[@source]
+
+      result = "<h4>#{@data["title"]}</h4>"
+      result += "<p>#{@data["description"]}</p>"
+      result += "<ul class=\"structured_html\">\n"
+      @data["items"].each {|item| result += _item(item)}
+      result += "</ul>"
+      result
+    end
+
+    private
+
+    def _item(item)
+      result = "<ul>\n"
+      result += "<li>\n"
+      result += "<code class=\"scss-var\">#{item["tag"]}</code> - #{item["description"]}\n"
+      item["children"].each{|child| result += _item(child)} if item["children"]
+      result += "</li>\n"
+      result += "</ul>\n"
+    end
+  end
+end
+
+Liquid::Template.register_tag('structured_html', Jekyll::StructuredHtmlTag)
